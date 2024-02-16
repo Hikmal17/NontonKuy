07f40718a99a4eba1927af68fd52bd53b5244eef
Hikmal17 <2211102441158@umkt.ac.id>
2024-02-17 05:09:01 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/nontonkuy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nontonkuy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_constants.dart
@@ -0,0 +1,4 @@
+class AppConstans {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = "f2a4c7eee8f4658e6dad9044814f28b2";
+}
